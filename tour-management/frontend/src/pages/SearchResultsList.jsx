99181235490f62639da80cfb7e0868782d7c7d67
Warnakulaisuru <warnakulaisuru@gmail.com>
2024-05-27 23:49:17 +0530

    add routes for project

---
+++ tour-management/frontend/src/pages/SearchResultsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchResultsList = () => {
+  return (
+    <div>SearchResultsList</div>
+  )
+}
+
+export default SearchResultsList
---
+++ tour-management/frontend/src/router/Router.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+
+const Router = () => {
+  return (
+    <div>Router</div>
+  )
+}
+
+export default Router
